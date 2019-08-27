2133332b212c94d0a8bb0c677d8dc22b6f5f06b8
Petr Pošvic <petr@posvic.cz>
2019-08-27 13:28:57 +0200

    Create scaffold tool for creating new project

---
+++ tools/scaffold.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+from os import mkdir, rename
+from shutil import copytree, ignore_patterns, rmtree
+import click
+import re
+import subprocess
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.group()
+def new():
+    pass
+
+
+@new.command()
+@click.argument('path')
+@click.argument('package')
+def project(path, package):
+    """Create new project from template
+    """
+    copytree('../', path, ignore=ignore_patterns(
+        '.git', '.gradle', '.idea', 'build', 'example',
+        '*.iml', '*.swp',
+    ))
+
+    base = path + '/src/app-skeleton'
+
+    # Rename package in all files
+    subprocess.call([
+        'find', path, '-type', 'f',
+        '-exec', 'sed', '-i', '-e', 's/wtf.qase.appskeleton.template/{}/g'.format(package), '{}', ';'
+    ])
+
+    # Delete :example project, rename :template project
+    do_in_file(
+        base + '/settings.gradle',
+        lambda c: re.sub(
+            r'\':template\'',
+            '\':app\'',
+            re.sub(r'(, )?\':example\'(, )?', '', c),
+        ),
+    )
+
+    # Create new package structure
+    for n in ['main', 'test']:
+        src = base + '/template/src/{}/kotlin/'.format(n)
+        parent = src
+        for d in package.split('.'):
+            parent += d + '/'
+            mkdir(parent)
+
+        # Move to new package structure
+        rename(src + '/wtf/qase/appskeleton/template', parent)
+        rmtree(src + '/wtf/')
+
+    # Get name from last directory name in path
+    name = path.rsplit('/')[-1]
+    rename(base + '/template', base + '/app')
+    rename(base, path + '/src/' + name)
+
+    print('Change path to SDK and NDK in ' + path + '/src/' + name + '/local.properties')
+
+
+@new.command()
+@click.argument('path')
+@click.argument('name')
+def fragment(path, name):
+    raise Exception('TODO: Implement me')
+    try:
+        m = re.match('package="([^"]+)"', subprocess.check_output(
+            'grep "package" ' + path + '/src/main/AndroidManifest.xml',
+            shell=True,
+        ).decode('utf-8').strip())
+        if m:
+            package = m.group(1)
+            print(package)
+    except subprocess.CalledProcessError as e:
+        print(e)
+
+
+def do_in_file(filename, fn):
+    """Read content of file, call callback function and write content to file
+    """
+    with open(filename, 'r') as f:
+        content = f.read()
+
+    content = fn(content)
+
+    with open(filename, 'w') as f:
+        f.write(content)
+
+
+if __name__ == '__main__':
+    cli()
